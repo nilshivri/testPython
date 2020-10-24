bc36c6efd7764738b01316ea5356a2f0c944e97c
nilshivri <tcbj.n4@googlemail.com>
2020-10-24 17:33:27 +0200

    Coursera Assignment 5.3

---
+++ main.py
@@ -0,0 +1,22 @@
+largest = None
+smallest = None
+
+while True:
+    num = input("Enter Number: ")
+    if num == "done":
+        break
+    try:
+        num = int(num)
+    except:
+        print("Invalid input")
+        continue
+    if largest == None:
+        largest = num
+    elif num > largest:
+        largest = num
+    if smallest == None:
+        smallest = num
+    elif num < smallest:
+        smallest = num
+print("Maximum is", largest)
+print("Minimum is", smallest)
